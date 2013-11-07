94367359e8da43f1a0d5c535d0fbe57c7b75d31e
Veronica <veronica.ginman@gmail.com>
2013-11-07 14:58:03 +0100

    Added fermat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.math.BigInteger;
 
@@ -11,13 +12,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		ArrayList<BigInteger> number = new ArrayList<BigInteger>();
 		ArrayList<LinkedList<String>> answers = new ArrayList<LinkedList<String>>();
+		BigInteger TWO = new BigInteger("2");
+		
 		
 		BufferedReader br = new BufferedReader(
 				new InputStreamReader(System.in));
 		
 		String line;
 		line = br.readLine();
-		while(line != null){//line.length() >= 1 && line != null){// && line != "\n") {
+		while(line.length() >= 1){//line != null ) {//    //I can't run it on my computer  with line != null but it is nessesary for KATTIS
 			BigInteger tmp = new BigInteger(line);
 			number.add(tmp);
 			line = br.readLine();
@@ -27,34 +30,66 @@ public class Main {
 			answers.add(new LinkedList<String>());
 			BigInteger newNumber = number.get(i);
 			
-			if(newNumber.isProbablePrime(6))
+			if(newNumber.isProbablePrime(2))
 			{
 				answers.get(i).add(newNumber.toString());
 				continue;				
 			}
-			if(newNumber.bitLength() > 54) {
+			if(newNumber.bitLength() > 70) {
 				answers.get(i).add("fail");
 				continue;
 			}
-			BigInteger start = new BigInteger("2");			
-			BigInteger factorNr = factor(newNumber, start);
 			
-			// Primtal
-			if(factorNr == BigInteger.ZERO) {
-				answers.get(i).add(newNumber.toString());
-				continue;
+			if(newNumber.mod(TWO).compareTo(BigInteger.ZERO) == 0 || newNumber.bitLength() < 50)
+			{
+				BigInteger start = new BigInteger("2");			
+				BigInteger factorNr = factor(newNumber, start);
+				
+				// Primtal
+				if(factorNr == BigInteger.ZERO) {
+					answers.get(i).add(newNumber.toString());
+					continue;
+				}
+	
+				while(factorNr != BigInteger.ZERO) {	
+					start = factorNr;
+					answers.get(i).add(factorNr.toString());
+					newNumber = newNumber.divide(factorNr);
+					
+					factorNr = factor(newNumber, start);
+				}			
+								
+				answers.get(i).add(newNumber.toString());				
 			}
-
-			while(factorNr != BigInteger.ZERO) {	
-				start = factorNr;
-				answers.get(i).add(factorNr.toString());
-				newNumber = newNumber.divide(factorNr);
-				
-				factorNr = factor(newNumber, start);
-				
-				
-			}	
-			answers.get(i).add(newNumber.toString());
+			
+			else
+			{
+				BigInteger[] smallerN = fermat(newNumber);
+				//System.out.println(smallerN[0] + " " + smallerN[1] + "|| ");
+				for(int j = 0; j < 2; j++)
+				{
+					BigInteger start = new BigInteger("2");			
+					BigInteger factorNr = factor(smallerN[j], start);
+					
+					// Primtal
+					if(factorNr == BigInteger.ZERO) {
+						answers.get(i).add(smallerN[j].toString());
+						continue;
+					}
+		
+					while(factorNr != BigInteger.ZERO) {	
+						start = factorNr;
+						answers.get(i).add(factorNr.toString());
+						smallerN[j] = smallerN[j].divide(factorNr);
+						
+						factorNr = factor(smallerN[j], start);
+						
+						
+					}
+					
+					answers.get(i).add(smallerN[j].toString());
+				}
+			}
 		}
 
 		
@@ -87,4 +122,43 @@ public class Main {
 		}
 		
 	}
+	
+	private static BigInteger[] fermat(BigInteger n)
+	{
+		BigInteger[] pair = new BigInteger[2];
+		
+		BigInteger TWO = new BigInteger("2");
+		BigInteger s =  new BigInteger("" + (int)Math.sqrt(n.doubleValue()));
+		BigInteger u = (s.multiply(TWO)).add(BigInteger.ONE);
+		BigInteger v = BigInteger.ONE;
+		BigInteger r = (s.pow(2)).subtract(n);
+		
+		while(r != BigInteger.ZERO)
+		{
+			while(r.compareTo(BigInteger.ZERO) > 0)
+			{
+				r = r.subtract(v);//  r-v;
+				v = v.add(TWO);//v+2;
+			}
+			
+			while(r.compareTo(BigInteger.ZERO) < 0)
+			{
+				r = r.add(u);
+				u = u.add(TWO);
+			}
+		}
+		
+		BigInteger a = (u.add(v).subtract(TWO)).divide(TWO);
+		pair[0] = a;
+		BigInteger b = (u.subtract(v)).divide(TWO);
+		pair[1] = b;
+		
+		return pair;
+		
+		
+	}
+	
+	
+	
+	
 } 
